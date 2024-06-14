28aa725e3d7d50d37311276b90e6466cd1aacfa9
Marco Fernandes <malf88@gmail.com>
2024-06-13 23:25:55 -0300

    Melhorias na estrutura.

--- QAraCasosTeste/Config/qara.php
+++ QAraCasosTeste/Config/qara.php
@@ -1,2 +1,21 @@
 <?php
-return [];
+return [
+    'menu' => [
+                'text' => 'QAra',
+                'key' => 'qara_index',
+                'icon'  => 'fas fa-robot',
+                'submenu' => [
+                    [
+                        'text' => 'Casos de testes',
+                        'key' => 'casos_teste_qara_index',
+                        'route'  => 'caso-teste.qara.index',
+                        'classes' => 'ml-4',
+                        'icon'  => 'fas fa-file-alt',
+                        'active' => ['projetos/qara/casos-teste/*'],
+                        'label' => 'Novo',
+                        'label_color' => 'success',
+                        'can'   => 'QARA_CASOS_TESTE_INSERIR'
+                    ]
+            ]
+        ]
+];
--- QAraCasosTeste/Providers/QAraServiceProvider.php
+++ QAraCasosTeste/Providers/QAraServiceProvider.php
@@ -2,18 +2,20 @@
 
 namespace App\Modules\QAraCasosTeste\Providers;
 
+use App\Modules\Projetos\Business\ProjetoBusiness;
 use App\Modules\Projetos\Contracts\Business\CasoTesteBusinessContract;
+use App\Modules\Projetos\Contracts\Business\ProjetoBusinessContract;
+use App\Modules\Projetos\Controllers\CasoTesteController;
 use App\Modules\Projetos\Providers\ProjetosServiceProvider;
 use App\Modules\QAraCasosTeste\Business\QAraCasosTesteBusiness;
-use App\Modules\QAraCasosTeste\Config\MenuConfig;
 use App\Modules\QAraCasosTeste\Contracts\Business\QAraCasosTesteBusinessContract;
-use App\Modules\QAraCasosTeste\Controllers\QAraController;
 use App\System\Impl\ServiceProviderAbstract;
 use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
 
-class QAraServiceProvider extends ServiceProviderAbstract implements DeferrableProvider
+class QAraServiceProvider extends ServiceProviderAbstract
 {
-    protected string $module_path = 'vendor/malf88/qa-kit-qara-caso-teste/QAraCasosTeste';
+    protected string $module_path = 'vendor/qacorp/qteste-qaract/QAraCasosTeste';
     protected string $prefix = 'qara';
     protected string $view_namespace = 'qara';
     public $bindings = [
@@ -24,6 +26,7 @@ class QAraServiceProvider extends ServiceProviderAbstract implements DeferrableP
      */
     public function register(): void
     {
+        $this->mergeConfigFrom(__DIR__.'/../Config/qara.php', $this->prefix);
 
     }
     public function boot(): void
@@ -33,18 +36,10 @@ class QAraServiceProvider extends ServiceProviderAbstract implements DeferrableP
                 __DIR__.'/../Config/qara.php' => config_path('qara.php'),
             ]);
         }
+
         $this->moduleExists(ProjetosServiceProvider::class);
-
-        MenuConfig::configureMenuModule();
-
         parent::boot();
     }
-    public function provides()
-    {
-        return [
-            CasoTesteBusinessContract::class,
-            QAraCasosTesteBusinessContract::class
-        ];
-    }
+
 
 }
