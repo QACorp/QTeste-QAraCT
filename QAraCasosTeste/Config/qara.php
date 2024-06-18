2e65f3ad1105e4eb39c520f62faa44d6e862eade
Marco Fernandes <malf88@gmail.com>
2024-06-18 00:08:04 -0300

    Alterado icone.

--- QAraCasosTeste/Config/qara.php
+++ QAraCasosTeste/Config/qara.php
@@ -6,6 +6,17 @@ return [
                 'icon'  => 'fas fa-robot',
                 'submenu' => [
                     [
+                        'text' => 'Configurações',
+                        'key' => 'casos_teste_qara_configuracao',
+                        'route'  => 'caso-teste.qara.configuracao',
+                        'icon'  => 'fas fa-cogs',
+                        'active' => ['qara/configuracao/*'],
+                        'label' => 'Novo',
+                        'label_color' => 'success',
+                        'can'   => 'QARA_CASOS_TESTE_INSERIR',
+                        'topnav_user' => true
+                    ],
+                    [
                         'text' => 'Casos de testes',
                         'key' => 'casos_teste_qara_index',
                         'route'  => 'caso-teste.qara.index',
@@ -14,18 +25,8 @@ return [
                         'label' => 'Novo',
                         'label_color' => 'success',
                         'can'   => 'QARA_CASOS_TESTE_INSERIR'
-                    ],
-                    [
-                        'text' => 'Configurações',
-                        'key' => 'casos_teste_qara_configuracao',
-                        'route'  => 'caso-teste.qara.configuracao',
-                        'icon'  => 'fas fa-file-alt',
-                        'active' => ['qara/configuracao/*'],
-                        'label' => 'Novo',
-                        'label_color' => 'success',
-                        'can'   => 'QARA_CASOS_TESTE_INSERIR',
-                        'topnav_user' => true
                     ]
+
 
             ]
         ]
